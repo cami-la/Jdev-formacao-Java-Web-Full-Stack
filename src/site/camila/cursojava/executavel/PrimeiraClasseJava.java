207bac99312041d46463a5b24a78546a66549c76
cami <cami@CAMI.HP>
2020-12-19 11:12:56 -0300

    util class 8.22 - Criando a lista dinâmica das disciplinas do aluno

--- src/site/camila/cursojava/executavel/PrimeiraClasseJava.java
+++ src/site/camila/cursojava/executavel/PrimeiraClasseJava.java
@@ -7,6 +7,7 @@ import site.camila.cursojava.classes.Disciplina;
 
 public class PrimeiraClasseJava {
 	public static void main(String[] args) {
+		Aluno aluno1 = new Aluno();
 		
 		String nome = JOptionPane.showInputDialog("Nome: ");
 		String idade = JOptionPane.showInputDialog("Idade: ");
@@ -18,17 +19,6 @@ public class PrimeiraClasseJava {
 		String dataMatricula = JOptionPane.showInputDialog("Data da Matricula: ");
 		String serie = JOptionPane.showInputDialog("Série ");
 		String escola = JOptionPane.showInputDialog("Escola: ");
-		/*String disciplina1 = JOptionPane.showInputDialog("Disciplina 1: ");
-		String nota1 = JOptionPane.showInputDialog("Nota1: ");
-		String disciplina2 = JOptionPane.showInputDialog("Disciplina 2: ");
-		String nota2 = JOptionPane.showInputDialog("Nota2: ");
-		String disciplina3 = JOptionPane.showInputDialog("Disciplina 3: ");
-		String nota3 = JOptionPane.showInputDialog("Nota3: ");
-		String disciplina4 = JOptionPane.showInputDialog("Disciplina 4: ");
-		String nota4 = JOptionPane.showInputDialog("Nota4: ");
-		*/
-
-		Aluno aluno1 = new Aluno();
 		
 		aluno1.setNome(nome);
 		aluno1.setIdade(Integer.valueOf(idade));
@@ -41,38 +31,22 @@ public class PrimeiraClasseJava {
 		aluno1.setSerieMatriculado(serie);
 		aluno1.setNomeEscola(escola);
 		
-		Disciplina disciplina1 = new Disciplina();
-		disciplina1.setDisciplina("Banco de Dados");
-		disciplina1.setNota(90);
-		
-		Disciplina disciplina2 = new Disciplina();
-		disciplina2.setDisciplina("Matemática");
-		disciplina2.setNota(80);
-		
-		Disciplina disciplina3 = new Disciplina();
-		disciplina3.setDisciplina("Geografia");
-		disciplina3.setNota(97);
-		
-		Disciplina disciplina4 = new Disciplina();
-		disciplina4.setDisciplina("Java Web");
-		disciplina4.setNota(70);
-		
-		aluno1.getDisciplinas().add(disciplina1);
-		aluno1.getDisciplinas().add(disciplina2);
-		aluno1.getDisciplinas().add(disciplina3);
-		aluno1.getDisciplinas().add(disciplina4);	
-		
-		
-		System.out.println("O nome do aluno1 é = " + aluno1.getNome());
-		System.out.println("A idade do aluno1 é = " + aluno1.getIdade() + " anos");
-		System.out.println("A data de nascimento do aluno1 é = " + aluno1.getDataNascimento());
-		System.out.println("Escola " + aluno1.getNomeEscola());
-		System.out.println("Média da nota do aluno1 é " + aluno1.getMediaNota());
-		System.out.println("Resultado = " + (aluno1.isAlunoAprovado() ? "Aprovado" : "Reprovado"));
-		System.out.println("Mensagem = " + aluno1.getAlunoAprovado());
-		System.out.println("====================");
+		for (int j = 1; j <= 2; j++) {
+			String nomeDisciplina = JOptionPane.showInputDialog("Diciplina " + j + ":");
+			String valorNota = JOptionPane.showInputDialog("Nota Disciplina " + j + ":");
+
+			Disciplina disciplinas = new Disciplina();
+			disciplinas.setDisciplina(nomeDisciplina);
+			disciplinas.setNota(Double.valueOf(valorNota));
+
+			aluno1.getDisciplinas().add(disciplinas);
+		}
 		
 		System.out.println(aluno1);
+		System.out.println("Média da nota do aluno = " + aluno1.getMediaNota());
+		//System.out.println("Resultado = " + (aluno1.isAlunoAprovado() ? "Aprovado" : "Reprovado"));
+		System.out.println("Resultado = " + aluno1.getAlunoAprovado());
+		System.out.println("------------------------------------------");
 		
 	}
 }

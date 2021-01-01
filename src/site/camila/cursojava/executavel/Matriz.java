61594b2030f5de630a206d82a7978456906254cc
cami <camiladsantoscavalcante@gmail.com>
2021-01-01 16:27:15 -0300

    util class 12.12 - Criando uma Matriz multidimensional

---
+++ src/site/camila/cursojava/executavel/Matriz.java
@@ -0,0 +1,18 @@
+package site.camila.cursojava.executavel;
+
+public class Matriz {
+	public static void main(String[] args) {
+		
+		int notas[][] = new int[2][3];
+		
+		notas[0][0] = 80;
+		notas[0][1] = 90;
+		notas[0][2] = 70;
+		
+		notas[1][0] = 40;
+		notas[1][1] = 60;
+		notas[1][2] = 30;
+
+		
+	}
+}

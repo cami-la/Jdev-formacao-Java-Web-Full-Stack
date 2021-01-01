7b9384c5bb2b98119cadf72fe06a486e2170141c
cami <camiladsantoscavalcante@gmail.com>
2021-01-01 16:39:49 -0300

    util class  12.13 - Lendo uma Matriz multidimensional

--- src/site/camila/cursojava/executavel/Matriz.java
+++ src/site/camila/cursojava/executavel/Matriz.java
@@ -12,7 +12,18 @@ public class Matriz {
 		notas[1][0] = 40;
 		notas[1][1] = 60;
 		notas[1][2] = 30;
-
+		
+		for (int i = 0; i < notas.length; i++) { //coluna
+			for (int j = 0; j < notas[i].length; j++) {
+				System.out.print(notas[i][j] + " ");
+			}
+			System.out.println();
+			
+			/*for (int nota : notas[i]) //linha
+				System.out.print(nota + " ");
+			System.out.println();
+			*/
+		}
 		
 	}
 }

9b8b5141edfa92d0925bd90fbfce3c21bb0f105b
cami <camiladsantoscavalcante@gmail.com>
2021-01-10 08:21:49 -0300

    util class 13.12 - Implementação do processo de Fila e Pilha com Thread

--- src/site/camila/cursojava/threads/classes/ImplementacaoFilaThread.java
+++ src/site/camila/cursojava/threads/classes/ImplementacaoFilaThread.java
@@ -1,15 +1,19 @@
 package site.camila.cursojava.threads.classes;
 
-import java.util.Iterator;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class ImplementacaoFilaThread extends Thread {
 	private static ConcurrentLinkedQueue<ObjetoFilaThread> pilhaFila = new ConcurrentLinkedQueue<ObjetoFilaThread>();
-
+	
+	public ImplementacaoFilaThread() {
+		new Thread(new TarefaProcessandoLista(pilhaFila)).start();
+	}
+	
 	public static void add(ObjetoFilaThread objetoFilaThread) {
 		pilhaFila.add(objetoFilaThread);
 	}
 	
+	/*
 	@Override
 	public void run() {
 		Iterator<ObjetoFilaThread> iteracao = pilhaFila.iterator(); //transformou o a pilhaFila em um objeto iterável
@@ -17,12 +21,12 @@ public class ImplementacaoFilaThread extends Thread {
 		while(iteracao.hasNext()) {
 			ObjetoFilaThread processar = iteracao.next(); //pega o objeto atual
 			
-			/*
+			
 			 Faz a rotina:
 			 - processar NF;
 			 - gera uma lista de pdf;
 			 - gera envio de e-mails; 
-			*/
+			
 			System.out.println("------------------");
 			System.out.println(processar.getEmail());
 			System.out.println(processar.getNome());
@@ -42,5 +46,5 @@ public class ImplementacaoFilaThread extends Thread {
 			e.printStackTrace();
 		} 
 	}
-
+	*/
 }
---
+++ src/site/camila/cursojava/threads/classes/TarefaProcessandoLista.java
@@ -0,0 +1,49 @@
+package site.camila.cursojava.threads.classes;
+
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class TarefaProcessandoLista implements Runnable {
+
+	private ConcurrentLinkedQueue<ObjetoFilaThread> pilhaFila;
+
+	public TarefaProcessandoLista(ConcurrentLinkedQueue<ObjetoFilaThread> pilhaFila) {
+		this.pilhaFila = pilhaFila;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Fila rodando...");
+
+		while (true) {
+			Iterator<ObjetoFilaThread> iteracao = pilhaFila.iterator(); // transformou o a pilhaFila em um objeto iterável
+			synchronized (iteracao) {
+				while (iteracao.hasNext()) {
+					ObjetoFilaThread processar = iteracao.next(); // pega o objeto atual
+
+					/*
+					 * Faz a rotina: - processar NF; - gera uma lista de pdf; - gera envio de
+					 * e-mails;
+					 */
+					System.out.println("------------------");
+					System.out.println(processar.getEmail());
+					System.out.println(processar.getNome());
+
+					iteracao.remove();
+
+					try {
+						Thread.sleep(100); // dar um tempo para descarga de memória
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+
+				try {
+					Thread.sleep(100); // dar um tempo para descarga de memória
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}

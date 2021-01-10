208b385a41a77a5041672016cc28c7e3a2fad983
cami <camiladsantoscavalcante@gmail.com>
2021-01-10 06:41:42 -0300

    util class 13.11 - Criando Fila e Pilha para processar paralelo Thread

---
+++ src/site/camila/cursojava/threads/classes/ImplementacaoFilaThread.java
@@ -0,0 +1,46 @@
+package site.camila.cursojava.threads.classes;
+
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class ImplementacaoFilaThread extends Thread {
+	private static ConcurrentLinkedQueue<ObjetoFilaThread> pilhaFila = new ConcurrentLinkedQueue<ObjetoFilaThread>();
+
+	public static void add(ObjetoFilaThread objetoFilaThread) {
+		pilhaFila.add(objetoFilaThread);
+	}
+	
+	@Override
+	public void run() {
+		Iterator<ObjetoFilaThread> iteracao = pilhaFila.iterator(); //transformou o a pilhaFila em um objeto iterável
+		
+		while(iteracao.hasNext()) {
+			ObjetoFilaThread processar = iteracao.next(); //pega o objeto atual
+			
+			/*
+			 Faz a rotina:
+			 - processar NF;
+			 - gera uma lista de pdf;
+			 - gera envio de e-mails; 
+			*/
+			System.out.println("------------------");
+			System.out.println(processar.getEmail());
+			System.out.println(processar.getNome());
+			
+			iteracao.remove();
+			
+			try {
+				Thread.sleep(100); //dar um tempo para descarga de memória
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} 
+		}
+		
+		try {
+			Thread.sleep(100); //dar um tempo para descarga de memória
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} 
+	}
+
+}
---
+++ src/site/camila/cursojava/threads/classes/ObjetoFilaThread.java
@@ -0,0 +1,49 @@
+package site.camila.cursojava.threads.classes;
+
+public class ObjetoFilaThread {
+	private String nome;
+	private String email;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ObjetoFilaThread other = (ObjetoFilaThread) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+}
